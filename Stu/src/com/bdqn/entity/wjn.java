7d06ca9cd088f09d9a5d61c07c178938ebcfb8bc
wjn <2043571979@qq.com>
2019-09-09 17:54:44 +0800

    2019-9-9   王江南 提交

--- Stu/src/com/bdqn/entity/wjn.java
+++ Stu/src/com/bdqn/entity/wjn.java
@@ -1,5 +1,13 @@
 package com.bdqn.entity;
 
 public class wjn {
+	private String name;
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
--- Stu/src/com/bdqn/test/wjnMain.java
+++ Stu/src/com/bdqn/test/wjnMain.java
@@ -1,5 +1,11 @@
 package com.bdqn.test;
 
-public class wjnMain {
+import com.bdqn.entity.wjn;
 
+public class wjnMain {
+	public static void main(String[] args) {
+		wjn w = new wjn();
+		w.setName("Íõ½­ÄÏ");
+		System.out.println(w.getName());
+	}
 }
